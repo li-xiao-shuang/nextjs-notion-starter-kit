58b41e4b7a5cf8d22cf6875e9c93061ab3464b3a
李晓双 Li Xiao Shuang <644968328@qq.com>
2022-11-17 13:28:30 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Blog-271ef232ca3a4c3ab0da172f34f1baec',
+  rootNotionPageId: 'blogs-b3ae269289ff4250942bf79e77a75201',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //  navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'About-dbd47f0834bf44568da2224de2e8641f'
+    },
+    {
+      title: 'Contact',
+      pageId: 'Contact-3dcb06e26a5541fc98490502afc727b7'
+    }
+  ]
 })
